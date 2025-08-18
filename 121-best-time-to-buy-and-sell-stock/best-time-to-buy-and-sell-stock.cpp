8926b515aec442f2ae1754b320439330edb6f024
nazmulislam <nazmulislam93400@gmail.com>
2025-08-18 15:04:44 +0600

    Time: 8 ms (6.58%) | Memory: 97.2 MB (86.90%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -5,10 +5,11 @@ public:
         int curr_profit = 0;
         int max_profit = 0;
         for (int i = 1; i < prices.size(); i++) {
-            buy_price = min(prices[i], buy_price);
-            if (prices[i] > buy_price)
-                curr_profit = prices[i] - buy_price;
-            max_profit = max(curr_profit, max_profit);
+            if(prices[i]<buy_price) buy_price = prices[i];
+            else{
+                curr_profit = prices[i]-buy_price;
+                max_profit = max(curr_profit,max_profit);
+            }
         }
         return max_profit;
     }
