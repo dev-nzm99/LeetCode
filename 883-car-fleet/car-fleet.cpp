78ae4ae78ad3656e7f316a5440608628e97d5a7f
nazmulislam <nazmulislam93400@gmail.com>
2025-10-08 23:37:44 +0600

    Time: 51 ms (50.60%) | Memory: 115.1 MB (17.98%) - LeetSync

---
+++ 883-car-fleet/car-fleet.cpp
@@ -0,0 +1,32 @@
+/*
+Author: Nazmul Islam
+Approach: Using Monotonic Increasing stack Logic 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size();
+        vector<pair<int, double>> cars;
+
+        for (int i = 0; i < n; i++) {
+            double time = (double)(target - position[i]) / speed[i];
+            cars.push_back({position[i], time});
+        }
+
+        sort(cars.begin(), cars.end(),
+             [](const pair<int, double> a, const pair<int, double> b) {
+                 return a.first > b.first;
+             });
+        stack<double> stk;
+        for (int i = 0; i < n; i++) {
+            double speed = cars[i].second;
+            if (!stk.empty() && speed <= stk.top()) {
+                continue;
+            }
+            stk.push(speed);
+        }
+        return stk.size();
+    }
+};
