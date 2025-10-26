9abd54a060a487ed7a15019eb6504a0d631b987c
nazmulislam <nazmulislam93400@gmail.com>
2025-10-26 16:40:11 +0600

    Time: 0 ms (100.00%) | Memory: 14.7 MB (58.92%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,32 @@
+/*
+ Author: Nazmul Islam
+ Created: 2025-10-26 15:59
+ Using Converging Two Pointers technique [O(n)]
+*/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<pair<int, int>> a(n);
+
+        for (int i = 0; i < n; ++i)
+            a[i] = {i, nums[i]};
+
+        // Sort by value (second)
+        sort(a.begin(), a.end(),
+             [](const pair<int, int>& x, const pair<int, int>& y) {
+                 return x.second < y.second;
+             });
+        int l = 0, r = n - 1;
+        while (l < r) {
+            int sum = a[l].second + a[r].second;
+            if (sum == target)
+                break;
+            else if (sum < target)
+                l++;
+            else
+                r--;
+        }
+        return {a[l].first, a[r].first};
+    }
+};
