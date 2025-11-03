7672af976b71bf931380fcfff9b518cae6d105ed
nazmulislam <nazmulislam93400@gmail.com>
2025-11-03 13:51:25 +0600

    Time: 0 ms (100.00%) | Memory: 7.9 MB (73.20%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,29 @@
+/*
+ Author:Nazmul Islam
+ Created: 2025-11-03 13:50
+ Complexity: O(n) 
+*/
+class Solution {
+public:
+    bool isHappy(int n) {
+        int fast, slow;
+        fast = slow = n;
+        do {
+            slow = find(slow);
+            fast = find(find(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+
+private:
+    int find(int val) {
+        int res = 0;
+        while (val > 0) {
+            int k = val % 10;
+            res += (k * k);
+            val /= 10;
+        }
+        return res;
+    }
+};
