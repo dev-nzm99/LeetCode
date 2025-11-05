af0ed397e0a985225159df13d4bf17cfdf7cee62
nazmulislam <nazmulislam93400@gmail.com>
2025-11-05 13:32:06 +0600

    Time: 3 ms (16.54%) | Memory: 23.8 MB (55.17%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,11 +1,21 @@
+/*
+ Author:Nazmul Islam
+ Using Two Pointers (In-Place Array Modification)
+ O(n) time O(1) space
+*/
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i=0,j=0;
-        while(i<nums.size()){
-            if(nums[i]!=0){
-                swap(nums[i],nums[j++]);
-            }i++;
+        int n = nums.size();
+        int write = 0;
+        for (int read = 0; read < n; ++read) {
+            if (nums[read] != 0) {
+                nums[write++] = nums[read];
+            }
+        }
+        // fill the rest with 0's
+        while (write < n) {
+            nums[write++] = 0;
         }
     }
 };
