20a4c1ce1dfea8b210bcd4b5d91e632683ad1324
nazmulislam <nazmulislam93400@gmail.com>
2025-11-12 23:35:50 +0600

    Update fruit-into-baskets.cpp

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -7,18 +7,16 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int, int> m;
+        unordered_map<int, int> basket;
         int res = INT_MIN;
         int l = 0;
         for (int r = 0; r < fruits.size(); ++r) {
-            m[fruits[r]]++;
-            if (m.size() > 2) {
-                while (l <= r && m.size() > 2) {
-                    m[fruits[l]]--;
-                    if (m[fruits[l]] == 0)
-                        m.erase(fruits[l]);
-                    ++l;
-                }
+            basket[fruits[r]]++;
+            while (basket.size() > 2) {
+                basket[fruits[l]]--;
+                if (basket[fruits[l]] == 0)
+                    basket.erase(fruits[l]);
+                ++l;
             }
             res = max(res, r - l + 1);
         }
