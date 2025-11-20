d981bf6df57f60cb983fefec8b1c57865311aa79
nazmulislam <nazmulislam93400@gmail.com>
2025-11-21 00:57:51 +0600

    Time: 13 ms (39.24%) | Memory: 36.9 MB (8.51%) - LeetSync

---
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,28 @@
+/*
+ Author:Nazmul Islam
+ Using prefix sum and Hash map 
+ COmplexity: O(n) time & O(n) Space
+*/
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& arr, int k) {
+        int n = arr.size();
+        vector<int> prefixSum(n, 0);
+        unordered_map<int, int> m;
+        prefixSum[0] = arr[0];
+        for (int i = 1; i < n; ++i)
+            prefixSum[i] = prefixSum[i - 1] + arr[i];
+        int res = 0;
+        m[0]++;
+        for (int i = 0; i < n; ++i) {
+            int mod = prefixSum[i] % k;
+            if (mod < 0) {
+                mod += k;
+            }
+            if (m.contains(mod))
+                res += m[mod];
+            m[mod]++;
+        }
+        return res;
+    }
+};
